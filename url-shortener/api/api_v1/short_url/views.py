db93d5d58ff239a5c155d1ac5a8d87b2b7837208
Irina <Irina-prokopova.style@yandex.ru>
2025-09-20 14:23:11 +0300

    Add post def create_short_url with Form and Annotated

--- url-shortener/api/api_v1/short_url/views.py
+++ url-shortener/api/api_v1/short_url/views.py
@@ -1,11 +1,15 @@
 from typing import Annotated
+
+from annotated_types import Len
 from fastapi import (
     HTTPException,
     status,
     Depends,
     APIRouter,
+    Form,
 )
 from fastapi.responses import RedirectResponse
+from pydantic import AnyHttpUrl
 
 from api.api_v1.short_url.crud import SHORT_URLS
 from api.api_v1.short_url.dependencies import prefetch_short_url
@@ -20,6 +24,18 @@ router = APIRouter(
 @router.get("/", response_model=list[ShortUrl])
 def read_short_urls_list():
     return SHORT_URLS
+
+
+@router.post(
+    "/",
+    response_model=ShortUrl,
+    status_code=status.HTTP_201_CREATED,
+)
+def create_short_url(
+    target_url: Annotated[AnyHttpUrl, Form()],
+    slug: Annotated[str, Len(min_length=3, max_length=100), Form()],
+) -> ShortUrl:
+    return ShortUrl(target_url=target_url, slug=slug)
 
 
 @router.get("/{slug}", response_model=ShortUrl)
