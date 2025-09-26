51a8b1c15541b2e0564f164be611d535eaafaab8
Irina <Irina-prokopova.style@yandex.ru>
2025-09-26 20:54:42 +0300

    Add core with config (Base_Dir, SHORT_URL_STORAGE_FILEPATH)

---
+++ url-shortener/core/config.py
@@ -0,0 +1,11 @@
+import logging
+from pathlib import Path
+
+BASE_DIR = Path(__file__).resolve().parent.parent
+SHORT_URL_STORAGE_FILEPATH = BASE_DIR / "short-url.json"
+
+# C:\Users\User\PycharmProjects\fastapi-url-shortener\url-shortener\core\config.py
+LOG_LEVEL = logging.INFO
+LOG_FORMAT: str = (
+    "[%(asctime)s.%(msecs)03d] %(module)10s:%(lineno)-3d %(levelname)-7s - %(message)s"
+)
