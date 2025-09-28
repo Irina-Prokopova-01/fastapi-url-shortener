0e62cd2c988870231a87a8502c85b2ad63f777f1
Irina <Irina-prokopova.style@yandex.ru>
2025-09-28 18:31:20 +0300

    Add lifespan

--- url-shortener/main.py
+++ url-shortener/main.py
@@ -1,23 +1,19 @@
 import logging
-from typing import Annotated
 from fastapi import (
     FastAPI,
     Request,
-    HTTPException,
-    status,
-    Depends,
 )
-from fastapi.responses import RedirectResponse
+
+from app_lifespan import lifespan
 from core import config
 
 from api import router as api_router
 from api.redirect_views import router as redirect_views
 
-from schemas.short_url import ShortUrl
-
 logging.basicConfig(level=config.LOG_LEVEL, format=config.LOG_FORMAT)
 app = FastAPI(
     title="URL Shortener",
+    lifespan=lifespan,
 )
 
 app.include_router(redirect_views)
