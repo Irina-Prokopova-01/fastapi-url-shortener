0c3b4368e4517c799f3410fbdecf2a10dd918210
Irina <Irina-prokopova.style@yandex.ru>
2025-10-31 13:55:50 +0300

    28.6 Add 2 tests for to catch errors (test_short_url_slug_to_regex, test_short_url_slug_to_short)

--- url-shortener/testing/test_schemas/test_short_url.py
+++ url-shortener/testing/test_schemas/test_short_url.py
@@ -1,5 +1,6 @@
 from unittest import TestCase
 
+from pydantic import ValidationError
 from typing_extensions import Annotated
 
 from schemas.short_url import (
@@ -73,20 +74,45 @@ class ShortUrlCreateTestCase(TestCase):
         urls = [
             "http://example.com",
             "https://example",
-            "rtmp://video.example.com",
-            "rtmps://video.example.com",
+            # "rtmp://video.example.com",
+            # "rtmps://video.example.com",
             "http://abc.example.com",
             "https://www.example.com/foobar/",
         ]
 
         for url in urls:
-            # with self.subTest(url=url, msg=f"test-url {url}"):
-            short_url_create = ShortUrlCreate(
-                slug="some_slug",
-                target_url=url,
+            with self.subTest(url=url, msg=f"test-url {url}"):
+                short_url_create = ShortUrlCreate(
+                    slug="some_slug",
+                    target_url=url,
+                    description="some_description",
+                )
+                self.assertEqual(
+                    url.rstrip("/"),
+                    short_url_create.model_dump(mode="json")["target_url"].rstrip("/"),
+                )
+
+    def test_short_url_slug_to_short(self) -> None:
+        with self.assertRaises(ValidationError) as exc_info:
+            ShortUrlCreate(
+                slug="s",
+                target_url="https://example.com",
                 description="some_description",
             )
-            self.assertEqual(
-                url.rstrip("/"),
-                short_url_create.model_dump(mode="json")["target_url"].rstrip("/"),
+        print(exc_info.exception)
+        print(exc_info.exception.json())
+        print(exc_info.exception.errors())
+        error_details = exc_info.exception.errors()[0]
+        expected_type = "string_too_short"
+        self.assertEqual(expected_type, error_details["type"])
+
+    def test_short_url_slug_to_regex(self) -> None:
+        with self.assertRaisesRegex(
+            ValidationError,
+            expected_regex="String should have at least 3 characters",
+        ):
+            ShortUrlCreate(
+                slug="s",
+                target_url="https://example.com",
+                description="some_description",
             )
