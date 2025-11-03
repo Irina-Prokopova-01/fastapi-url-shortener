fd61602522e39042461d8f5230808e1fa0942df4
Irina <Irina-prokopova.style@yandex.ru>
2025-11-03 19:53:22 +0300

    28.11 Add pre-setup tests in test_crud.py (ShortUrlStorageUpdateTestCase)

--- url-shortener/testing/test_api/test_api_v1/test_short_urls/test_crud.py
+++ url-shortener/testing/test_api/test_api_v1/test_short_urls/test_crud.py
@@ -1,22 +1,77 @@
 import random
+import string
 from unittest import TestCase
 from os import getenv
+
+from api.api_v1.short_url.crud import storage
+from schemas import short_url
+from schemas.short_url import (
+    ShortUrl,
+    ShortUrlUpdate,
+    ShortUrlCreate,
+    ShortUrlPartialUpdate,
+)
 
 if getenv("TESTING") != "1":
     raise OSError(  # noqa: TRY003
         "Environment is not ready for testing",  # noqa: EM101
     )
-#
-#
-# def total(a: int, b: int) -> int:
-#     return a + b
-#
-#
-# class TotalTestCase(TestCase):
-#     def test_total(self) -> None:
-#         num_a = random.randint(1, 100)
-#         num_b = random.randint(1, 100)
-#         result = total(num_a, num_b)
-#         # print("result:", result)
-#         exported_result = num_a + num_b
-#         self.assertEqual(exported_result, result)
+
+
+class ShortUrlStorageUpdateTestCase(TestCase):
+    def setUp(self) -> None:
+        self.short_url = self.create_short_url()
+
+    def tearDown(self) -> None:
+        storage.delete(self.short_url)
+
+    def create_short_url(self) -> ShortUrl:
+        short_url_in = ShortUrlCreate(
+            slug="".join(
+                random.choices(
+                    string.ascii_uppercase + string.digits,
+                    k=8,
+                )
+            ),
+            description="A short url",
+            target_url="https://example.com",
+        )
+        return storage.create(short_url_in)
+
+    def test_update(self) -> None:
+        short_url = self.create_short_url()
+        short_url_update = ShortUrlUpdate(
+            **self.short_url.model_dump(),
+        )
+        source_description = self.short_url.description
+        short_url_update.description *= 2
+        updated_short_url = storage.update(
+            short_url=self.short_url,
+            short_url_in=short_url_update,
+        )
+        self.assertNotEqual(
+            source_description,
+            updated_short_url.description,
+        )
+        self.assertEqual(
+            short_url_update,
+            ShortUrlUpdate(**updated_short_url.model_dump()),
+        )
+
+    def test_update_partial(self) -> None:
+        short_url_partial_update = ShortUrlPartialUpdate(
+            description=self.short_url.description * 2,
+        )
+        source_description = self.short_url.description
+        updated_short_url = storage.update_partial(
+            short_url=self.short_url,
+            short_url_in=short_url_partial_update,
+        )
+        self.assertNotEqual(
+            source_description,
+            updated_short_url.description,
+        )
+        self.assertEqual(
+            short_url_partial_update.description,
+            updated_short_url.description,
+        )
